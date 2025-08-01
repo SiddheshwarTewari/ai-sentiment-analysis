c7bdbe67e281087d01072c8a0d97f9680fa28e42
SiddheshwarTewari <sidcertifications@outlook.com>
2025-07-31 20:51:21 -0700

    static

--- app/main.py
+++ app/main.py
@@ -11,7 +11,6 @@ app = FastAPI()
 # Configure template directory path
 current_dir = os.path.dirname(os.path.abspath(__file__))
 templates = Jinja2Templates(directory=os.path.join(current_dir, "templates"))
-app.mount("/static", StaticFiles(directory=os.path.join(current_dir, "static")), name="static")
 
 # Download required NLTK data
 nltk.download('vader_lexicon')
